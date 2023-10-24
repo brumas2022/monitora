31e6e1a46710d59e3883eca45345b9299ac7d4cd
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-24 11:10:11 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -10,63 +10,64 @@ poco = st.selectbox("Escolha o poço", ("P41","P42","P64","P03","P59","P58","P04
 if poco == "P41":
         st.markdown("O nivel do P41 é 6,96m")
         id = 68
-elif poco == "/P42":
+elif poco == "P42":
         id = 71
-elif poco == "/P64":
+elif poco == "P64":
         id = 1
-elif poco == "/P03":
+elif poco == "P03":
         id = 2
-elif poco == "/P59":
+elif poco == "P59":
         id = 50
-elif poco == "/P58":
+elif poco == "P58":
         id = 52
-elif poco == "/P04":
+elif poco == "P04":
         id = 67
-elif poco == "/P34":
+elif poco == "P34":
         id = 69
-elif poco == "/P54":
+elif poco == "P54":
         id = 70
-elif poco == "/P45":
+elif poco == "P45":
         id = 72
-elif poco == "/P32":
+elif poco == "P32":
         id = 75
-elif poco == "/P55":
+elif poco == "P55":
         id = 80
-elif poco == "/P51":
+elif poco == "P51":
         id = 100
-elif poco == "/P30":
+elif poco == "P30":
         id = 101
-elif poco == "/P01":
+elif poco == "P01":
         id = 102
-elif poco == "/P07":
+elif poco == "P07":
         id = 103
-elif poco == "/P23":
+elif poco == "P23":
         id = 104
-elif poco == "/P61":
+elif poco == "P61":
         id = 108
 
-try:
-    connection = psycopg2.connect(
-        host = '192.168.0.169',
-        user = 'postgres',
-        password = 'cetis',
-        database = 'dbsitec',
-        port = '5432'
-     )
-    st.write("conexao exitosa")
-    cursor = connection.cursor()
+def consulta:
+    try:
+        connection = psycopg2.connect(
+            host = '192.168.0.169',
+            user = 'postgres',
+            password = 'cetis',
+            database = 'dbsitec',
+            port = '5432'
+             )
+        st.write("conexao exitosa")
+        cursor = connection.cursor()
         
-    comando = f"SELECT * FROM saneamento.registro_grandezas WHERE id_amb = {id} and cd_classe = 10  ORDER BY id DESC LIMIT 2"
+        comando = f"SELECT * FROM saneamento.registro_grandezas WHERE id_amb = {id} and cd_classe = 10  ORDER BY id DESC LIMIT 2"
 
-    cursor.execute(comando)
-    resultado = cursor.fetchone()
-    st.write(resultado)
+        cursor.execute(comando)
+        resultado = cursor.fetchone()
+        st.write(resultado)
 
-except Exception as ex:
+    except Exception as ex:
 
-    st.write(ex)
+        st.write(ex)
 
-
+st.markdowwn("**Este é o id do poço escolhido**')
 
 
 
